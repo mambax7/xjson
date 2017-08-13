6c11d344b5302e3eb1ba495f2aae6337ab6fc747
mambax7 <mambax7@gmail.com>
2017-08-12 23:21:00 -0400

    updates

---
+++ class/class.functions.php
@@ -0,0 +1,82 @@
+<?php
+
+class FunctionsHandler
+{
+    public $functions = array();
+
+    public function __construct($wsdl)
+    {
+    }
+
+    public function GetServerExtensions()
+    {
+        $files = array();
+        $f     = array();
+        $files = $this->getFileListAsArray(XOOPS_ROOT_PATH . '/modules/xjson/plugins/');
+        static $f_count;
+        static $f_buffer;
+
+        if ($f_count != count($files)) {
+            $f_count = count($files);
+            foreach ($files as $k => $l) {
+                if (strpos($k, ".php", 1) == (strlen($k) - 4)) {
+                    $f[] = $k;
+                }
+            }
+            $f_buffer = $f;
+        }
+
+        return $f_buffer;
+    }
+
+    public function getDirListAsArray($dirname)
+    {
+        $ignored = array();
+        $list    = array();
+        if (substr($dirname, -1) != '/') {
+            $dirname .= '/';
+        }
+        if ($handle = opendir($dirname)) {
+            while ($file = readdir($handle)) {
+                if (substr($file, 0, 1) == '.' || in_array(strtolower($file), $ignored)) {
+                    continue;
+                }
+                if (is_dir($dirname . $file)) {
+                    $list[$file] = $file;
+                }
+            }
+            closedir($handle);
+            asort($list);
+            reset($list);
+        }
+        //print_r($list);
+        return $list;
+    }
+
+    /*
+     *  gets list of all files in a directory
+     */
+    public function getFileListAsArray($dirname, $prefix = "")
+    {
+        $filelist = array();
+        if (substr($dirname, -1) == '/') {
+            $dirname = substr($dirname, 0, -1);
+        }
+        if (is_dir($dirname) && $handle = opendir($dirname)) {
+            while (false !== ($file = readdir($handle))) {
+                if (!preg_match("/^[\.]{1,2}$/", $file) && is_file($dirname . '/' . $file)) {
+                    $file            = $prefix . $file;
+                    $filelist[$file] = $file;
+                }
+            }
+            closedir($handle);
+            asort($filelist);
+            reset($filelist);
+        }
+        return $filelist;
+    }
+
+    public function __destruct()
+    {
+    }
+}
---
+++ language/english/main.php
@@ -0,0 +1,2 @@
+<?php
+//define('_C_FILENOTFOUND','File not found! Please check the URL!');
